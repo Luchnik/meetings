a6a6316362017e7915916922a183a4154f243063
Andriy Luchko <andruluchko@ukr.net>
2016-03-08 17:53:35 +0200

    Added basic controllers

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ myApp.config(['$routeProvider', function($routeProvider) {
     when('/login', {
       templateUrl: 'views/login.html',
       controller: 'RegistrationController'
-    }),
+    }).
     when('/register', {
       templateUrl: 'views/register.html',
       controller: 'RegistrationController'
-    }),
+    }).
     when('/success', {
       templateUrl: 'views/success.html',
       controller: 'SuccessController'
-    }),
+    }).
     otherwise({
       redirectTo: '/login'
-    })
+    });
 }]);
